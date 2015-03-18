97fc463371e36e5452241fce0ee0019c61d80396
Nilesh Kumbhar <nilesh.kumbhar@consult.nordea.com>
2015-03-18 17:37:47 +0530

    integrated spring security partly

---
+++ academix-web/src/main/java/com/xinov/academix/web/login/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.xinov.academix.web.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+
+	/*@RequestMapping(method=RequestMethod.GET)
+	public void login(@ModelAttribute )*/
+}
